bc9e95bd50cb194677757534025b0404686ceb45
the-widget <brennenawana108ny@gmail.com>
2016-03-04 18:16:10 -0500

    #

--- lib/run_swim_hike_nyc.rb
+++ lib/run_swim_hike_nyc.rb
@@ -6,3 +6,6 @@ require_relative "./run_swim_hike_nyc/swim_scraper"
 require_relative "./run_swim_hike_nyc/swim"
 require_relative "./run_swim_hike_nyc/hike_scraper"
 require_relative "./run_swim_hike_nyc/hike"
+
+module RunSwimHike
+end
